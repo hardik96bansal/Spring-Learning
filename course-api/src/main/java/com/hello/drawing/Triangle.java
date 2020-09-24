741eefa941212db2a27adf18cdefb47664a842ca
hardik <hardik96bansal@gmail.com>
2020-09-24 08:55:44 +0530

    Dependency Injection Start

---
+++ course-api/src/main/java/com/hello/drawing/Triangle.java
@@ -0,0 +1,18 @@
+package com.hello.drawing;
+
+public class Triangle {
+    
+    private String type;
+
+    public void draw(){
+        System.out.println(getType() + " Triangle Drawn");
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
